ba6684dc8c31b73d15639db7890550da36645014
Li Jun <875241499@qq.com>
2018-07-05 16:16:28 +0800

    StatChart Component

--- code/fe/src/App.js
+++ code/fe/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StatChart from './components/StatChart'
 
 class App extends Component {
   render() {
+    let mock_data = [
+      {time: '2018-08-09 20:30:11', value: 5},
+      {time: '2018-08-09 20:35:14', value: 6},
+      {time: '2018-08-09 20:40:40', value: 8},
+      {time: '2018-08-09 20:45:40', value: 2},
+      {time: '2018-08-09 20:50:40', value: 9},
+      {time: '2018-08-09 20:55:40', value: 3},
+      {time: '2018-08-09 21:00:40', value: 6},
+      {time: '2018-08-09 21:05:40', value: 5},
+      {time: '2018-08-09 21:10:40', value: 1},
+      {time: '2018-08-09 21:15:40', value: 2},
+      {time: '2018-08-09 21:20:40', value: 7},
+      {time: '2018-08-09 21:25:40', value: 8},
+      {time: '2018-08-10 21:40:40', value: 200}
+    ]
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <StatChart style={{
+          width: '600px',
+          height: '400px',
+          float: 'left'
+        }} color='#4169E1' type='line' title='Stat' data={mock_data} yValueName='人脸数'/>
+        <StatChart style={{
+          width: '600px',
+          height: '400px',
+          float: 'right'
+        }} color='#4169E1' type='bar' title='Stat' data={mock_data} yValueName='人脸数'/>
+        <StatChart data={mock_data}/>
       </div>
     );
   }
---
+++ code/fe/src/components/StatChart.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import ReactEcharts from 'echarts-for-react';
+import PropTypes from 'prop-types';
+
+class StatChart extends Component {
+    extractFormattedTime(timeString) {
+        let timePattern = /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/;
+        let resArr = timePattern.exec(timeString);
+        if (!resArr) {
+            return {};
+        }
+        let fields = ['year', 'month', 'day', 'hour', 'minute', 'second']
+        return Object.assign({}, ...(fields.map(function (field, index) {
+            return {[field]: resArr[index+1]}
+        })))
+    }
+
+    constructTimeString(timeObject) {
+        let year = timeObject.year ? timeObject.year + '-' : '';
+        let month = timeObject.month ? timeObject.month + '-' : '';
+        let day = timeObject.day ? timeObject.day + '\n' : '';
+        let hour = timeObject.hour ? timeObject.hour + ':' : '';
+        let minute = timeObject.minute ? timeObject.minute + ':' : '';
+        let second = timeObject.second ? timeObject.second : '';
+        return year + month + day + hour + minute + second;
+    }
+
+    extractXAxis(data) {
+        let timeArr = data.map((p) => (p.time));
+        let curTime;
+        timeArr.forEach((time, idx) => {
+            let timeObject = this.extractFormattedTime(time);
+            let timeObjectSave = Object.assign({}, timeObject);
+            if (curTime) {
+                if (curTime.year === timeObject.year) {
+                    delete timeObject.year;
+                    if (curTime.month === timeObject.month) {
+                        delete timeObject.month;
+                        if (curTime.day === timeObject.day) {
+                            delete timeObject.day;
+                            if (curTime.hour === timeObject.hour) {
+                                delete timeObject.hour;
+                                if (curTime.minute === timeObject.minute) {
+                                    delete timeObject.minute;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            timeArr[idx] = this.constructTimeString(timeObject);
+            curTime = timeObjectSave;
+        });
+        return timeArr;
+    }
+
+    getOption() {
+        let opt = {
+            title: {
+                text: this.props.title
+            },
+            tooltip: {},
+            xAxis: {
+                type: 'category',
+                data: this.extractXAxis(this.props.data),
+                axisLabel: {
+                    interval: 0,
+                    rotate: -30
+                }
+            }
+            ,
+            yAxis: {},
+            series: [{
+                name: this.props.yValueName,
+                type: this.props.type,
+                data: this.props.data,
+                itemStyle: {
+                    color: this.props.color
+                }
+            }]
+        };
+        return opt;
+    }
+
+    render() {
+        return (
+            <div style={this.props.style}>
+                <ReactEcharts option={this.getOption()} />
+            </div>
+        )
+    }
+}
+
+StatChart.propTypes = {
+    title: PropTypes.string,
+    type: PropTypes.oneOf(['line', 'bar']),
+    data: PropTypes.arrayOf(PropTypes.shape({
+        time: PropTypes.string,
+        value: PropTypes.number
+    })),
+    style: PropTypes.object,
+    color: PropTypes.string,
+    yValueName: PropTypes.string
+}
+
+StatChart.defaultProps = {
+    title: '统计',
+    type: 'bar',
+    style: {
+        width: '600px',
+        height: '400px'
+    },
+    color: '#4169E1',
+    yValueName: '人脸数'
+}
+
+export default StatChart;
