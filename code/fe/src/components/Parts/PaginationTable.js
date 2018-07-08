d8b72b3ebdf1ebe0219a7d5cf79e4d726f2c8f7b
DESKTOP-0O0KUAD\msi <hyq961206@sjtu.edu.cn>
2018-07-08 15:36:57 +0800

    standardize the code

--- code/fe/src/components/Parts/PaginationTable.js
+++ code/fe/src/components/Parts/PaginationTable.js
@@ -3,53 +3,56 @@ import { Table, Button, Form, Input } from 'antd';
 import PropTypes from 'prop-types'
 import{ Link } from 'react-router-dom'
 
-/**
- * input props:
- * size={"default"}/size={"middle"}/size={"small"} change the size of the table
- * (bordered={true}) to show border
- * title={"this is title"} to add a title
- * pageSize={10} to make 10 objects to be showed in a page
- * enablePage=true to enable pagination
- * path = {path} path contains a key and a path ,it shows the address linked to
- * column={columns} the header of the table
- * data={data} the body of the table
- *
- */
 
+/* Time: 2018/7/7
+ * Author: HuangYouqi
+ * Params:
+ *     size: change the size of the table(default/middle/small)
+ *     bordered: show border(true/false)
+ *     title: add title ("the title name")
+ *     pageSize: the num of the objects to be showed in a page (num of page)
+ *     enablePage: a switch to enable pagination(true/false)
+ *     enableSearch: a switch to enable search bar(true/false)
+ *     column: the header of the table,an array of arrays which contain 2 elements, title(the name of the column) and link(true to make it to a link)
+ *     data: the body of the table,an array of arrays which contain (x+1) elements,the number of elements defined in header(x) and id(+1)
+ * Intro: This component shows a pagination table with customized columns and data
+ * Return: a table component
+ */
 class PaginationTable extends Component {
 
-    static defaultProps={
+    static defaultProps = {
         bordered: false,
-        pageSize:10,
-        size:"default",
+        pageSize: 10,
+        size: 'default',
         title: undefined,
         enablePage: true,
         enableSearchBar: true,
-        showHeader:true,
+        showHeader: true
     };
+
     static propTypes ={
         bordered: PropTypes.bool,
-        enablePage:PropTypes.bool,
-        enableSearchBar:PropTypes.bool,
-        pagination:PropTypes.object,
+        enablePage: PropTypes.bool,
+        enableSearchBar: PropTypes.bool,
+        pagination: PropTypes.object,
         size: PropTypes.string,
         title: PropTypes.string,
-        column:PropTypes.array,
-        data:PropTypes.array
+        column: PropTypes.array,
+        data: PropTypes.array
     };
+
     constructor(props){
         super(props);
         let column = [];
-        //let path = this.props.path;
         this.props.column.forEach((columnItem) => {
             let aColumn = {
                 title: columnItem.title,
                 dataIndex: columnItem.title.toLowerCase(),
                 key: columnItem.title.toLowerCase()
             };
-            if(columnItem.type==='link') {
+            if(columnItem.type === 'link') {
                 Object.assign(aColumn, {
-                    render: (text, record, index) => <Link to={'/'+text/*path[index].pathName*/}>{text}</Link>
+                    render: (text, record, index) => <Link to={'/' + text /*path[index].pathName*/}>{text}</Link>
                 });
             }
             column.push(aColumn);
@@ -57,42 +60,47 @@ class PaginationTable extends Component {
         this.state = {
             bordered: this.props.bordered,
             loading: true,
-            pagination:{position: this.props.enablePage? "bottom":"none",pageSize:this.props.pageSize},
+            pagination: {position: this.props.enablePage ? 'bottom' : 'none', pageSize: this.props.pageSize},
             size: this.props.size,
-            title: this.props.title ? this.getTitle.bind(this):undefined,
-            columns:column,
+            title: this.props.title ? this.getTitle.bind(this) : undefined,
+            columns: column,
             dataSource:this.props.data
         };
     }
+
     componentWillMount(){
         this.setState({
             loading:true
-        })
+        });
     }
+
     componentDidMount(){
         this.setState({
             loading:false
-        })
+        });
     }
-    getTitle (){
+
+    getTitle(){
         return this.props.title;
     };
 
-    handleChange = (e) =>{
+    handleChange = (e) => {
         this.setState({
-            presearchData : e.target.value
-        })
+            preSearchData : e.target.value
+        });
     };
 
-    _toggleSearch = () =>{
-        if(!this.state.presearchData){
-            this.setState({dataSource: this.props.data});
+    toggleSearch = () => {
+        if(!this.state.preSearchData){
+            this.setState({
+                dataSource: this.props.data
+            });
             return;
         }
-        let needle = this.state.presearchData.toLowerCase();
+        let needle = this.state.preSearchData.toLowerCase();
         let searchData = this.props.data.filter(
             (row) => {
-                return row.name.toString().toLowerCase().indexOf(needle)>-1;
+                return row.name.toString().toLowerCase().indexOf(needle) > -1;
             }
         );
         this.setState({
@@ -100,21 +108,24 @@ class PaginationTable extends Component {
         });
     };
 
-    restore = () =>{
+    /**
+     * restore the table to the unfiltered one
+     */
+    restore = () => {
         this.setState({
             dataSource : this.props.data
         });
     };
 
-    render() {
+    render(){
         return (
             <div>
-                {this.props.enableSearchBar? <Form layout="inline">
+                {this.props.enableSearchBar ? <Form layout="inline">
                     <Form.Item>
                         <Input placeholder="请输入名称" onChange={this.handleChange}/>
                     </Form.Item>
                     <Form.Item>
-                        <Button type="primary" onClick={this._toggleSearch}>搜索</Button>
+                        <Button type="primary" onClick={this.toggleSearch}>搜索</Button>
                     </Form.Item>
                     <Form.Item>
                         <Button type="primary" onClick={this.restore}>还原</Button>
